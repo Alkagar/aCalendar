e34e45d9a20459cf31d8e332a1348b9a4caeba20
Alkagar <alkagar@gmail.com>
2012-08-29 18:08:16 +0200

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ACalendar::Application.config.secret_token = '9ce21ce309bcae319e3cda8ad65112c6754b6646d9157c309a5e956d7ec7eb9c56d7208fed6b0562bc1d2c2626b70d5781e71cd064e0dae491790d889e47ce00'
