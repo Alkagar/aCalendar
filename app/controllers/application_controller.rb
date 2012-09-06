a38f2dfc2b4548154c916826111a5d679f4027c2
Alkagar <alkagar@gmail.com>
2012-09-06 07:02:42 +0200

    add autologin based on cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery
 
-    before_filter :set_locale
+    before_filter :set_locale, :user_autologin
 
     def set_locale
         I18n.locale = params[:locale] || I18n.default_locale
+    end
+
+    def user_autologin
+        if cookies[:user_id]
+            logger.info 'try to autologin'
+            user = User.where(:id => cookies[:user_id]).first;
+            logger.info user.inspect
+            unless user.nil?
+                session[:current_user] = user
+                cookies[:user_id] = { :value => user.id, :expires => Time.now + 72 * 3600 } if user.autologin
+            end
+        end
     end
 
     def default_url_options(options={})
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,11 @@ class UsersController < ApplicationController
             if @user.nil?
                 flash.now[:error] = t(:general_form_wrong_login)
             else
+                if params[:user][:autologin]
+                    cookies[:user_id] = { :value => @user.id, :expires => Time.now + 72 * 3600 }
+                end
+                @user.autologin = params[:user][:password];
+                @user.save
                 session[:current_user] = @user
                 redirect_to(root_path)
             end
@@ -24,12 +29,14 @@ class UsersController < ApplicationController
     def create
         @user = User.new params[:user]
         if @user.valid? :on => :create
+            cookies[:user_id] = { :value => @user.id, :expires => Time.now + 72 * 3600 } if @user.should_autologin
             flash[:notice] = t(:general_flash_account_created)
             redirect_to(login_users_path) if @user.save
         end
     end
 
     def logout
+        cookies.delete :user_id
         session[:current_user] = nil
         redirect_to(root_path)
     end
