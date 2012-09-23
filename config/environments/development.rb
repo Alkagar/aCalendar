e2444f4397ddd1f70f42431f37ef78f5dd3e90c4
Alkagar <alkagar@gmail.com>
2012-09-23 09:28:23 +0200

    change log level to warn

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ ACalendar::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.log_level = :warn # In any environment initializer, or
 end
